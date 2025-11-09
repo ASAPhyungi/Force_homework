0d5f34f0309b7f939876b7dacc7258b542f28a93
ASAPhyungi <gusrl0423@gmail.com>
2025-11-09 23:41:37 +0900

    화면 아래로 떨어 트리기

--- sketch.js
+++ sketch.js
@@ -11,7 +11,9 @@ function draw() {
   background(220);
 
   let gravity = createVector(0.3, 0);
+  let gravity1 = createVector(0.3, 0.3);
   ball.addForce(gravity);
+  ball.addForce(gravity1);
 
   ball.update();
   ball.show();
